91fba4ca0a3557992bdf5a8a7dc045b944a3f86e
Tabanca34 <81834126+Tabanca34@users.noreply.github.com>
2021-04-04 18:13:08 +0300

    Update deneme1.java
    Gidhup. comdaki yeni versiyonumu ekliyorum

--- src/mygitproject/deneme1.java
+++ src/mygitproject/deneme1.java
@@ -3,7 +3,8 @@ package mygitproject;
 public class deneme1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("uzak sunucudaki kodum");// Githup .com daki yeni kodum
+		
 
 	}
 
