0a6070dbab5231db184a3b41ab673b94f0fce3de
Tabanca34 <81834126+Tabanca34@users.noreply.github.com>
2021-04-04 20:14:55 +0300

    Update deneme1.java

--- src/mygitproject/deneme1.java
+++ src/mygitproject/deneme1.java
@@ -4,7 +4,7 @@ public class deneme1 {
 
 	public static void main(String[] args) {
 		System.out.println("uzak sunucudaki kodum");// Githup .com daki yeni kodum
-		System.out.println("bu satırı ekledim");
+		System.out.println("bu satiri ekledim");
 		
 
 	}
